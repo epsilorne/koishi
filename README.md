a8be12eb67df67922e0041f043074bd0db8ccebf
Danh <32991333+epsilorne@users.noreply.github.com>
2025-06-11 23:38:53 +1000

    refac: build.py, add doco

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 # koishi
 *koishi* is a simple and barebones static site generator.
 
-More doco TBA.
+> [!WARNING]
+> This project is a WIP and generated results may not be as expected.
+
+## Installation
+*koishi* requires [Python-Markdown](https://pypi.org/project/Markdown/) to run.
+
+```bash
+pip install markdown
+git clone https://github.com/epsilorne/koishi.git
+cd koishi
+```
+
+## Usage
+
+Markdown files are used to contain the content of webpages.
+*koishi* will recursively search for `.md` files in the `input/` directory.
+The default title for a page will be `DEFAULTTITLE`, but this can be changed by adding `$<CUSTOM TITLE>` to the first line of the markdown file.
+
+To generate the site, run:
+```bash
+python build.py
+```
+
+The generated `.html` files will be located in the `output/` directory.
+
+## Configuration
+*koishi* can be customised in `conf.ini`:
+
+```ini
+[koishi]
+# The default title of the page
+title = DEFAULTTITLE
+
+# Template .html and .css files
+html=template/default.html
+css=template/style.css
+
+# Input .md and output .html directories
+input_dir=input/
+output_dir=output/
+```
+
+Furthermore, you can configure the template `.html` and `.css` files to your liking.
+*koishi* injects the markdown contents, page title and stylesheet to wherever `$BODY`, `$TITLE` and `$CSS` are located, so you can use your own `.html` and have *koishi* insert the contents for you.
--- build.py
+++ build.py
@@ -10,22 +10,17 @@ config = configparser.ConfigParser()
 html_template: str
 css_template: str
 title_template: str
-
-INPUT_DIR = "input/"
-
-def load_files():
-    for f in glob.glob(INPUT_DIR + '**/*.md', recursive=True):
-        with open(f, 'r') as file:     
-            generate_html(file)
+input_dir: str
+output_dir: str
 
 def generate_html(file: typing.TextIO):
-    global title_template, html_template, css_template
+    global title_template, html_template, css_template, input_dir, output_dir
     page_title: str = title_template
     lines: list[str] = []
 
     # If the first line starts with $, we set that as the page title
     for i, line in enumerate(file):
-        if i == 0 and line[0] == '$':
+        if i == 0 and line[0] == '$' and len(line) > 1:
             page_title = line[1:].strip()
         else:
             lines.append(line)
@@ -38,22 +33,25 @@ def generate_html(file: typing.TextIO):
     contents = contents.replace("$TITLE", page_title)
     contents = contents.replace("$BODY", body)
 
-    output_file_title: str = file.name.replace("input", "output").replace("md", "html")
-    
+    output_name: str = file.name.replace(input_dir, output_dir).replace("md", "html")
+
     # Make the output file (and directories if they do not exist)
-    output_file = Path(output_file_title)
+    output_file = Path(output_name)
     output_file.parent.mkdir(exist_ok=True, parents=True)
     output_file.write_text(contents)
 
     print(f"Generated page: {file.name}")
 
 def main():
-    global config, title_template, html_template, css_template
-    config.read("settings.ini")
+    global config, title_template, html_template, css_template, input_dir, output_dir
 
+    # Parse the config file
+    config.read("conf.ini")
     title_template = config['koishi']['title']
     html_template = config['koishi']['html']
     css_template = config['koishi']['css']
+    input_dir = config['koishi']['input_dir']
+    output_dir = config['koishi']['output_dir']
 
     # Load the .html and .css templates into memory
     try:
@@ -67,8 +65,12 @@ def main():
         sys.stderr.write("Template .html and/or .css files were not found! Check settings.ini to ensure the paths are valid and the files exist.")
         exit()
 
-    load_files()
-    print("Finished building website! Check output/ for the result.")
+    # Recursively look for .md files to parse
+    for f in glob.glob(input_dir+ '**/*.md', recursive=True):
+        with open(f, 'r') as file:
+            generate_html(file)
+
+    print(f"Finished building website! Check {output_dir} for the result.")
 
 if __name__ == "__main__":
     main()
