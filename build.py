bb73fdbb8a4964a10722a164bf05e3100cf61a5c
Danh <32991333+epsilorne@users.noreply.github.com>
2025-06-14 13:39:05 +1000

    add: custom injectable modules

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import configparser
 import glob
+import os
 import markdown
 from pathlib import Path
 import typing
@@ -13,8 +14,11 @@ title_template: str
 input_dir: str
 output_dir: str
 
+custom_modules: dict[str, str] = {}
+md_files: list[str] = []
+
 def generate_html(file: typing.TextIO):
-    global title_template, html_template, css_template, input_dir, output_dir
+    global title_template, html_template, css_template, input_dir, output_dir, custom_modules
     page_title: str = title_template
     lines: list[str] = []
 
@@ -32,6 +36,10 @@ def generate_html(file: typing.TextIO):
     contents: str = html_template
     contents = contents.replace("$TITLE", page_title)
     contents = contents.replace("$BODY", body)
+
+    for module in custom_modules:
+        replace_tag: str = f"$INSERT {module}"
+        contents = contents.replace(replace_tag, custom_modules[module])
 
     output_name: str = file.name.replace(input_dir, output_dir).replace("md", "html")
 
@@ -56,18 +64,27 @@ def main():
     # Load the .html and .css templates into memory
     try:
         with open(html_template, 'r') as file:
-            html_template = ' '.join(file)
+            html_template = ''.join(file)
 
         with open(css_template, 'r') as file:
-            css_template = ' '.join(file)
+            css_template = ''.join(file)
 
     except:
         sys.stderr.write("Template .html and/or .css files were not found! Check settings.ini to ensure the paths are valid and the files exist.")
         exit()
 
-    # Recursively look for .md files to parse
-    for f in glob.glob(input_dir+ '**/*.md', recursive=True):
-        with open(f, 'r') as file:
+    # Recursively look for .md and .html files to parse
+    for f in glob.glob(input_dir+ '**/*', recursive=True):
+        if f.endswith(".md"):
+            md_files.append(f)
+        elif f.endswith(".html"):
+            with open(f, 'r') as file:
+                # We only add the filename (not any directories)
+                custom_modules[os.path.basename(f)] = ''.join(file)
+
+    # Then, generate the HMTL files by parsing the .md
+    for file in md_files:
+        with open(file, 'r') as file:
             generate_html(file)
 
     print(f"Finished building website! Check {output_dir} for the result.")
